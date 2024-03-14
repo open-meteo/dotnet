28ec07ab81e688ba2e680c4a66048f49749352cc
patrick-zippenfenig <patrick@zippenfenig.de>
2024-03-14 10:35:42 +0100

    init c#

---
+++ OpenMeteo.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace OpenMeteo.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ OpenMeteo/OpenMeteo.cs
@@ -0,0 +1,6 @@
+﻿namespace OpenMeteo;
+
+public class OpenMeteo
+{
+
+}
